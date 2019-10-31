628a9738ff3965c6279640f9be82ea98552f66cc
JoaoGFarias <jgfarias42@gmail.com>
2019-10-31 17:32:53 +0100

    Calculator with mutants

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        calculator.add(2);
+        Assert.assertEquals(calculator.getResult(), 2);
+    }
+
+    @Test
+    public void testPower() {
+        Calculator calculator = new Calculator(2);
+        calculator.power(3);
+        Assert.assertEquals(calculator.getResult(), 8);
+    }
+
+    @Test
+    public void testConditionalSetTrue() {
+        Calculator calculator = new Calculator();
+        Assert.assertTrue(calculator.setConditional(2, true));
+    }
+
+    @Test
+    public void testConditionalSetFalse() {
+        Calculator calculator = new Calculator();
+        Assert.assertFalse(calculator.setConditional(3, false));
+    }
+}
