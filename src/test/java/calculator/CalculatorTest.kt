a73acced06af9a94154df5d88a5cbfc49aba25c8
JoaoGFarias <joao@thatsabug.com>
2020-04-13 11:24:10 +0200

    AssertJ

--- src/test/java/calculator/CalculatorTest.kt
+++ src/test/java/calculator/CalculatorTest.kt
@@ -1,34 +1,33 @@
 package calculator
 
-import org.junit.Assert
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class CalculatorTest {
-
 
     @Test
     fun testAddition() {
         val calculator = Calculator()
         calculator.add(2.0)
-        Assert.assertEquals(calculator.getResult().toLong(), 2)
+        assertThat(calculator.getResult()).isEqualTo(2.0)
     }
 
     @Test
     fun testPower() {
         val calculator = Calculator(displayedValue = 2.0)
         calculator.power(3.0)
-        Assert.assertEquals(calculator.getResult().toLong(), 8)
+        assertThat(calculator.getResult()).isEqualTo(8.0)
     }
 
     @Test
     fun testConditionalSetTrue() {
         val calculator = Calculator()
-        Assert.assertTrue(calculator.tryToSetDisplay(2.0, true))
+        assertThat(calculator.tryToSetDisplay(2.0, true)).isTrue()
     }
 
     @Test
     fun testConditionalSetFalse() {
         val calculator = Calculator()
-        Assert.assertFalse(calculator.tryToSetDisplay(3.0, false))
+        assertThat(calculator.tryToSetDisplay(2.0, false)).isFalse()
     }
 }
